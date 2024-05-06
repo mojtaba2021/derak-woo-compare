69b1d601461648781683aeb8a50b569b767c721a
mojtaba mohammadi <52585886+mojtaba2021@users.noreply.github.com>
2024-05-07 03:07:29 +0430

    Add compare button to product single page

--- public/partials/compare-functions.php
+++ public/partials/compare-functions.php
@@ -16,3 +16,14 @@ function wcpc_add_compare_button_to_shop_loop()
     echo "</div>";
 }
 add_action('woocommerce_before_shop_loop_item_title', 'wcpc_add_compare_button_to_shop_loop');
+
+//  Add compare button to single product page
+function wcpc_add_compare_button_product_single_page()
+{
+    global $product;
+    $product_id = $product->get_id();
+    echo "<span class='dwpc-card-buttons-single-product'>";
+    echo "<a  href='" . esc_url(home_url('compare')) . "' class='dwpc-btn-compare'  data-product-id='$product_id'>مقایسه</a>";
+    echo "</span>";
+}
+add_action('woocommerce_single_product_summary', 'wcpc_add_compare_button_product_single_page');
